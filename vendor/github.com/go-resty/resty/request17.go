346f7d49b51d41f57b31f9f5f2fa60c245615271
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-07-06 17:41:07 -0400

    vendor bump

--- vendor/github.com/go-resty/resty/request17.go
+++ vendor/github.com/go-resty/resty/request17.go
@@ -49,7 +49,7 @@ type Request struct {
 	client              *Client
 	bodyBuf             *bytes.Buffer
 	multipartFiles      []*File
-	multipartFields     []*MultipartField
+	multipartFields     []*multipartField
 }
 
 // Context method returns the Context if its already set in request
